29c013f8dce3308086b9edc73cc0865a256e884f
nechaeva-irina <ivn@tut.by>
2022-05-30 19:46:18 +0300

    Test data is generated and loaded from JSON file

--- conftest.py
+++ conftest.py
@@ -1,32 +1,53 @@
+import importlib
+import jsonpickle
 import pytest
 from appium import webdriver
 import json
+import os.path
 
 target = None
+fixture = None
 
-@pytest.fixture(scope='session')
-def des_cap():
-  with open('data/desired_capabilities.json') as desired_capabilities:
-    data = json.load(desired_capabilities)
-  return data
 
 @pytest.fixture(scope="session")
 def init_driver(request):
     global fixture
     global target
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
     fixture = webdriver.Remote('http://localhost:4723/wd/hub', desired_capabilities=target["iOS"])
     return fixture
 
 
 @pytest.fixture(scope="session", autouse=True)
-def tearDown(request):
+def stop(request):
     def fin():
         fixture.quit()
+
     yield
     request.addfinalizer(fin)
 
+
 def pytest_addoption(parser):
     parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+        elif fixture.startswith("json_"):
+            testdata = load_from_json(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" % module).testdata
+
+
+def load_from_json(file):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
---
+++ data/switches_data.py
@@ -0,0 +1,4 @@
+from model.testdata_model import Values
+
+testdata = [
+    Values(switches_value=0, steppers_value=10, sliders_value=1, picker_red=80, picker_blue=100, picker_green=200)]
